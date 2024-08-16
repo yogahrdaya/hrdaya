78c9445328d964c3a439df1ad531be7c0c810aa0
dev-tinkerbell <dev.tinkerbell@gmail.com>
2024-08-17 01:16:13 +0900

    mobile - studio 레이아웃 수정

--- src/app/studio/page.tsx
+++ src/app/studio/page.tsx
@@ -5,8 +5,8 @@ import LinkButtons from "../ui/linkButtons";
 
 function LayoutTop({ images }: { images: number[] }) {
   return (
-    <div className="mt-[20px] flex gap-[20px]">
-      <div className="relative h-[600px] w-full max-w-[400px] overflow-hidden">
+    <div className="mt-[20px] flex flex-col gap-[20px] md:flex-row">
+      <div className="relative h-[calc(100vw*1.32)] w-full md:h-[600px] md:max-w-[400px]">
         <Image
           src={`${nextConfig.basePath}/studio_0${images[0]}.jpg`}
           fill
@@ -15,7 +15,7 @@ function LayoutTop({ images }: { images: number[] }) {
           className="object-cover"
         />
       </div>
-      <div className="relative h-[546px] w-full max-w-[820px] overflow-hidden">
+      <div className="relative h-[calc(100vw*0.6)] w-full md:h-[546px] md:max-w-[820px]">
         <Image
           src={`${nextConfig.basePath}/studio_0${images[1]}.jpg`}
           fill
@@ -30,8 +30,8 @@ function LayoutTop({ images }: { images: number[] }) {
 
 function LayoutBottom({ images }: { images: number[] }) {
   return (
-    <div className="mt-[-34px] flex items-end gap-[20px]">
-      <div className="relative h-[546px] w-full max-w-[820px] overflow-hidden">
+    <div className="mt-[20px] flex flex-col gap-[20px] md:mt-[-34px] md:flex-row md:items-end">
+      <div className="relative h-[calc(100vw*0.6)] w-full md:h-[546px] md:max-w-[820px]">
         <Image
           src={`${nextConfig.basePath}/studio_0${images[0]}.jpg`}
           fill
@@ -40,7 +40,7 @@ function LayoutBottom({ images }: { images: number[] }) {
           className="object-cover"
         />
       </div>
-      <div className="relative h-[600px] w-full max-w-[400px] overflow-hidden">
+      <div className="relative h-[calc(100vw*1.32)] w-full md:h-[600px] md:max-w-[400px]">
         <Image
           src={`${nextConfig.basePath}/studio_0${images[1]}.jpg`}
           fill
@@ -56,16 +56,18 @@ function LayoutBottom({ images }: { images: number[] }) {
 export default function Page() {
   return (
     <SubLayout imageSrc="studio_kv.jpg" title="스튜디오 소개">
-      <div className="flex w-full items-center justify-between border-t border-black pt-[43px]">
-        <h3 className="text-title">
+      <div className="flex w-full items-center justify-between border-black md:border-t md:pt-[43px]">
+        <h3 className="text-[26px] md:text-title">
           <p className="font-bold">
             <span className="font-serif">HRDAYA YOGA</span> 스튜디오
           </p>
         </h3>
-        <LinkButtons />
+        <div className="hidden md:block">
+          <LinkButtons />
+        </div>
       </div>
 
-      <div className="mt-[93px]">
+      <div className="mt-[30px] md:mt-[93px]">
         <LayoutTop images={[1, 2]} />
         <LayoutBottom images={[3, 4]} />
         <LayoutTop images={[5, 6]} />

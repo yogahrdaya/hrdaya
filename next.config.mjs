ff1479ce81d3c7757200a77fbf43ba3b3c7cb751
dev-tinkerbell <dev.tinkerbell@gmail.com>
2024-08-20 22:44:38 +0900

    nextConfig 수정

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  assetPrefix: isProd ? "/hrdaya/" : "",
-  basePath: isProd ? "/hrdaya" : "",
+  assetPrefix: "",
+  basePath: "",
 };
 
 export default nextConfig;
